ec7740723e7a6001620860862937a2dbe5b2dc06
Vivek Dange <135146295+captain-vivek@users.noreply.github.com>
2024-08-23 06:59:51 +0000

    added commment

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,8 +5,24 @@
 #include <iostream>
 #include <string>
 
+
+enum class cow_purpose {dairy, meat, hide, pet};
+
+struct cow{
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+
 int main(){
+    cow my_cow;
     
-    std::cout << std::endl << std::endl;
+    my_cow.name = "bella";
+    my_cow.age=5;
+    my_cow.purpose = cow_purpose::dairy;
+
+    std::cout << "my cow purpose is of "<<(int)my_cow.purpose<<std::endl;
+    std::cout<<"my cow name is"<<my_cow.name<<std::endl;
+    std::cout<<"my cow age is"<<my_cow.age<<std::endl;
     return (0);
 }
